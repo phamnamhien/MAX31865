06c55046c6a7be8c77b55efc5f7fb9e77c1b042c
phamnamhien <48169003+phamnamhien@users.noreply.github.com>
2025-08-08 08:25:36 +0700

    delete an intermediate folder
    MAX31865/MAX31865/Core -> MAX31865/Core

---
+++ Debug/MAX31865/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (13.3.rel1)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../MAX31865/max31865.c 
+
+OBJS += \
+./MAX31865/max31865.o 
+
+C_DEPS += \
+./MAX31865/max31865.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+MAX31865/%.o MAX31865/%.su MAX31865/%.cyclo: ../MAX31865/%.c MAX31865/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m3 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F103xB -c -I../Core/Inc -I../Drivers/STM32F1xx_HAL_Driver/Inc -I../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F1xx/Include -I../Drivers/CMSIS/Include -I"D:/Documents/GitHub/MAX31865/MAX31865" -Oz -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+
+clean: clean-MAX31865
+
+clean-MAX31865:
+	-$(RM) ./MAX31865/max31865.cyclo ./MAX31865/max31865.d ./MAX31865/max31865.o ./MAX31865/max31865.su
+
+.PHONY: clean-MAX31865
+
